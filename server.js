a383d277bad19355136ba4d4db3ef938a2ba5a20
Glitch (sticky-warp-eclipse) <none>
2022-04-17 22:53:50 +0530

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/api", function ech(req, res) {
 });
 
 app.get("/api/:date", function ech(req, res) {
-  var date = new Date(req.params.date);
+  var date = new Date(parseInt(req.params.date));
   if (isNaN(date.valueOf())) {
     res.json({ error: "Invalid Date" });
   }
